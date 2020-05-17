dbf535859b0468c0eb9af754e20470663deb7f92
Bryan Aguilar <b.aguilar4@gmail.com>
2020-05-16 19:24:35 -0700

    First Commit

---
+++ ChickenCrossing.py
@@ -0,0 +1,254 @@
+import pygame
+import pygame.display
+import random
+import pygame.draw
+from pygame.locals import(
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+pygame.init()
+#constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 500
+PLAYER_SIZE = (25,25)
+#our clock to control frame rate
+clock = pygame.time.Clock()
+#screen draw
+screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
+#arrow key list
+ARROW_KEYS = [K_DOWN,K_UP,K_LEFT,K_RIGHT]
+
+#position of first conveyor belt
+FIRST_CONVEYOR_LINE = (0,200)
+
+CONVEYOR_STARTING_X = 0
+CONVEYOR_STARTING_Y = 200
+
+#our player class
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player,self).__init__()
+        self.surf = pygame.Surface(PLAYER_SIZE)
+        self.surf.fill((255,255,255))
+        self.rect = self.surf.get_rect()
+    
+    def update(self,key_event):
+        if key_event == K_UP:
+            self.rect.move_ip(0,-25)
+        elif key_event == K_DOWN:
+            self.rect.move_ip(0,25)
+        elif key_event == K_RIGHT:
+            self.rect.move_ip(25,0)
+        elif key_event == K_LEFT:
+            self.rect.move_ip(-25,0)
+            
+        #bounds checking
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT  
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        
+
+class EnemyBlock(pygame.sprite.Sprite):
+    #This is our enemy block class
+    #The only values it holds is the size and speed at which it moves
+    #Currently the size is fixed at 25 units high and the width is passed in
+    #Width is stored individually for each conveyor belt in the conveyor belt class
+    
+    
+    def __init__(self,conveyor_stats):
+        super(EnemyBlock,self).__init__()
+        self.surf = pygame.Surface((conveyor_stats.block_width,25))
+        #make it red
+        self.surf.fill((255,0,0))
+        #Puts our rectangle starting location based on the direction it is supposed to be moving
+        if conveyor_stats.direction == 1:
+            #speed 
+            self.speed = conveyor_stats.block_speed   
+            self.rect = self.surf.get_rect(
+                left = conveyor_stats.location[0]-conveyor_stats.block_width,
+                top =conveyor_stats.location[1]
+            )
+        elif conveyor_stats.direction == -1:
+            #negates block speed to move right to left
+            self.speed = conveyor_stats.block_speed*-1
+            self.rect = self.surf.get_rect(
+                left = (conveyor_stats.location[0]+1000)+conveyor_stats.block_width,
+                top = conveyor_stats.location[1]
+            )
+        
+
+    def update(self):
+        #we will just be moving to the right
+        self.rect.move_ip(self.speed,0)
+        #bounds checking
+        #Uses the speed as a reference. If moving right to left speed will always be negative
+        if self.speed > 0:
+            if self.rect.left >= SCREEN_WIDTH:
+                self.kill()
+        else:
+            if self.rect.right < 0:
+                self.kill()
+
+
+class ConveyorBelt():
+    #TODO Get rid of setters if they are not doing any type of input validation. This is more pythonic
+    def __init__(self):
+        #default values
+        self.block_speed=1
+        #1 is right -1 is left
+        self.direction = 1
+        #block size?
+        #may not be needed here
+        block_width = 25
+        #how often blocks are made on belt
+        self.timing = 3000
+
+        #location of the loop
+        self.location = (0,0)
+
+    def setBlockSpeed(self,bs):
+        self.block_speed = bs
+    
+    def setDirection(self,direct):
+        #if blocks move left to right value is 1
+        #if blocks move right to left value is -1
+        if direct == 1 or direct == -1:
+            self.direction = direct
+    
+    def setTiming(self,timing):
+        #how often blocks will be created on the line. 
+        #default value is 3000ms
+        self.timing = timing
+    def setLocation(self,x,y):
+        self.location = (x,y)
+    
+    def setBlockWidth(self,w):
+        self.block_width = w
+
+
+def create_conv_list(num,cl):
+    for i in range(num):
+        cl.append(ConveyorBelt())
+
+def initConveyors(conveyor_list):
+    #this funciton populates our conveyor object attributes
+    direction = 1
+    x = CONVEYOR_STARTING_X
+    y = CONVEYOR_STARTING_Y
+    for c in conveyor_list:
+        #set location and increment y value
+        c.setLocation(x,y)
+        y += 25
+        #set direction and flip
+        c.setDirection(direction)
+        direction *= -1
+        #set block speed
+        rand_speed = random.randint(5,20)
+        c.setBlockSpeed(rand_speed)
+        #set timing
+       
+        c.setTiming(random.randint(1000,6000))
+        #set block width
+        w = random.randint(25,150)
+        c.setBlockWidth(w)
+
+def createCustomEvents(e_list,num):
+    #create custom events for our conveyor lines
+    #our e_list[0] will refer to first conveyor line
+    #e_list[1] will refer to second conveyor line
+    #FIXME Custom user events can only go from 25-32 because anything more will cause timing issues.
+    #TODO Maybe need to re-evaluate how custom events are handled 
+    for i in range(num):
+        e_list.append(pygame.USEREVENT + (i+1))
+
+
+def main():
+
+    #determine how many lines
+    num_conv = 7
+    #conv_list object contains all of our conveyor line objects
+    #conv_list[0] will be the top most line
+    #conv_list[1] will be the one directly below that
+    conv_list = []
+    #see createcustom event method for doc
+    event_list = [] 
+    create_conv_list(num_conv,conv_list)
+    initConveyors(conv_list)
+    createCustomEvents(event_list,num_conv)
+
+
+    #This loop will create our custom timers for each row
+    for i in range(num_conv):
+        pygame.time.set_timer(event_list[i],conv_list[i].timing)
+
+    #create our player
+    player = Player()
+
+    #sprite groups
+    all_sprites = pygame.sprite.Group()
+    conveyor_blocks = pygame.sprite.Group()
+    all_sprites.add(player)
+
+    running = True
+
+    #conveyor belt background
+  
+    
+    while running:
+        #background color
+        screen.fill((0,0,0))
+        #draw safe space
+        first_conv_rect = (FIRST_CONVEYOR_LINE,(SCREEN_WIDTH,25*num_conv))
+        screen.fill((0,255,0),first_conv_rect)
+
+        #event loop
+        for event in pygame.event.get():
+            
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                #check if it was an arrow key. these are the only ones we want to pass to our player
+                if event.key in ARROW_KEYS:
+                    player.update(event.key)
+            #Checks if the event is a custom event
+            elif event.type in event_list:
+                #creates an index to use when referencing our conveyor object list
+                list_index = event_list.index(event.type)  
+                #create new stuff
+                new_enemy_block = EnemyBlock(conv_list[list_index])
+                conveyor_blocks.add(new_enemy_block)
+                all_sprites.add(new_enemy_block)
+
+        #update all of our auto update
+        conveyor_blocks.update()
+
+        #draw sprites
+        for entity in all_sprites:
+            screen.blit(entity.surf,entity.rect)
+
+        #collision check
+        if pygame.sprite.spritecollideany(player, conveyor_blocks):
+            # If so, then remove the player and stop the loop
+                player.kill()
+                running = False
+
+        pygame.display.flip()
+        
+        clock.tick(30)
+
+    pygame.quit()
+
+
+
+
+main()
